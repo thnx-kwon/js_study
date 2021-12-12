56040ce72c930595b81e5a217c561764e7b83feb
thnx-kwon <83857466+thnx-kwon@users.noreply.github.com>
2021-12-12 21:38:15 +0900

    stream 실습

---
+++ nodejs_study/buffer.js
@@ -0,0 +1,12 @@
+const buffer = Buffer.from('저를 버퍼로 바꿔보세요.');
+
+console.log(buffer);
+console.log(buffer.length);
+console.log(buffer.toString());
+console.log(buffer.toString().length);
+
+const array = [Buffer.from('띄엄 '), Buffer.from('띄엄 '), Buffer.from('띄어쓰기'), ]
+console.log(array.length);
+console.log(Buffer.concat(array).toString());
+
+console.log(Buffer.alloc(5));
