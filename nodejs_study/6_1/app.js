b2b20032a966ad13e5fce1cac73dde38a58bdbf0
thnx-kwon <83857466+thnx-kwon@users.noreply.github.com>
2022-01-02 17:55:32 +0900

    6장 실습

--- nodejs_study/6_1/app.js
+++ nodejs_study/6_1/app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const path = require('path');
-
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
+
 app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('combined'));
+app.use('/', express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'somethingPassword',
+  cookie:{
+    httpOnly: true,
+  },
+  name: 'connect.sid',
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
 app.use((req, rest, next) => {
   console.log('모든 요청에 실행하고 싶어요');
@@ -10,8 +28,19 @@ app.use((req, rest, next) => {
 });
 
 app.get('/', (req, res) => {
-  // res.send('Hello, Express');
+  req.cookies;
+  req.signedCookies;
+  res.cookie('name', encodeURIComponent('name'), {
+    expires: new Date(),
+    httpOnly: true,
+    path: '/',
+  });
+  // res.clearCookie('name', encodeURIComponent('name'), {
+  //   httpOnly: true,
+  //   path: '/',
+  // });
   res.sendFile(path.join(__dirname, '/index.html'));
+
 });
 
 app.get('/about', (req, res) => {
